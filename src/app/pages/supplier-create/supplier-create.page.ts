a63a985133826ebb9f72052bf1c72c59aab07f7e
Unknown <damicogiuseppe77@gmail.com>
2018-12-27 20:22:34 +0100

    question-switch
    creato campo question-switch

--- src/app/pages/supplier-create/supplier-create.page.ts
+++ src/app/pages/supplier-create/supplier-create.page.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
-import {QuestionBase} from '../../modules/item/models/question-base';
-import {TextboxQuestion} from '../../modules/item/models/question-textbox';
-import {DropdownQuestion} from '../../modules/item/models/question-dropdown';
+import { QuestionBase } from '../../modules/item/models/question-base';
+import { TextboxQuestion } from '../../modules/item/models/question-textbox';
+import { DropdownQuestion } from '../../modules/item/models/question-dropdown';
+import { SwitchQuestion } from '../../modules/item/models/question-swith';
 
 
 @Component({
@@ -16,32 +17,40 @@ export class SupplierCreatePage implements OnInit {
   constructor() {
     const questions: QuestionBase<any>[] = [
 
-      new DropdownQuestion({
-        key: 'brave',
-        label: 'Bravery Rating',
-        options: [
-          {key: 'solid',  value: 'Solid'},
-          {key: 'great',  value: 'Great'},
-          {key: 'good',   value: 'Good'},
-          {key: 'unproven', value: 'Unproven'}
-        ],
-        order: 3
-      }),
+      /* new DropdownQuestion({
+         key: 'brave',
+         label: 'Bravery Rating',
+         options: [
+           {key: 'solid',  value: 'Solid'},
+           {key: 'great',  value: 'Great'},
+           {key: 'good',   value: 'Good'},
+           {key: 'unproven', value: 'Unproven'}
+         ],
+         order: 3
+       }),*/
 
       new TextboxQuestion({
-        key: 'firstName',
-        label: 'First name',
+        key: 'title',
+        label: 'Nome del Fornitore',
         value: 'Bombasto',
         required: true,
         order: 1
       }),
-
       new TextboxQuestion({
-        key: 'emailAddress',
-        label: 'Email',
-        type: 'email',
+        key: 'indirizzo',
+        label: 'indirizzo',
+        value: 'Bombasto',
+        required: false,
         order: 2
+      }),
+      new SwitchQuestion({
+        key: 'online',
+        label: 'venditore online',
+        value: false,
+        required: false,
+        order: 3
       })
+
     ];
     this.questions = questions.sort((a, b) => a.order - b.order);
   }
