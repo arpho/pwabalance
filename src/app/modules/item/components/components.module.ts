ed338a8a022dcccb3f9f012ba41768bfc0a0fb8b
Unknown <damicogiuseppe77@gmail.com>
2018-12-31 12:10:22 +0100

    cleaned up
    removed old depencies

--- src/app/modules/item/components/components.module.ts
+++ src/app/modules/item/components/components.module.ts
@@ -6,8 +6,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { ComponentsPage } from './components.page';
-import { QuestionFormComponent } from './question-form-component/question-form-component.component';
-import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
 
 const routes: Routes = [
   {
